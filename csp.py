3e3b902fd05a412e9f9738b0bd9788f334e952d1
Sabariakash22009103 <akashsabari002@gmail.com>
2024-10-16 17:05:36 +0530

    Added

--- csp.py
+++ csp.py
@@ -8,27 +8,22 @@ Original file is located at
 """
 
 from itertools import permutations
-
 def solve_cryptarithmetic():
     for perm in permutations(range(10), 8):
         S, E, N, D, M, O, R, Y = perm
-
         # Check for leading zeros
-        TYPE THE CODE HERE USING IF
+        if S == 0 or M == 0:
             continue
-
         # Check the equation constraints
         SEND = 1000 * S + 100 * E + 10 * N + D
         MORE = 1000 * M + 100 * O + 10 * R + E
         MONEY = 10000 * M + 1000 * O + 100 * N + 10 * E + Y
-
         if SEND + MORE == MONEY:
             return SEND, MORE, MONEY
-
     return None
 
-solution = TYPE THE CODE HERE #CALL THE FUNCTION
-
+# Call the function
+solution = solve_cryptarithmetic()
 if solution:
     SEND, MORE, MONEY = solution
     print(f'SEND = {SEND}')
